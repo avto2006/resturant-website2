7bf5e85f747835f068703fbbcecd203494586592
Avto Bagaturia <Avtobagaturia21@gmail.com>
2025-12-22 19:50:05 +0400

    some

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 const alt = document.querySelector('.alt').addEventListener('click', () => {
-    alert('Please call this number: 995-579-85-45-31')
+    alert('Please call this number: 579-85-45-31')
 })
